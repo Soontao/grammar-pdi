7d0ef9ecb3a8e17f7f7fcae490b18582c60b75f0
Theo Sun <theo.sun@sap.com>
2019-11-07 15:18:10 +0800

    fix: ci script

--- absl/make_lib.sh
+++ absl/make_lib.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# alias
-if ! type "antlr" > /dev/null; then
-  if type "antlr4" > /dev/null; then 
-    alias antlr=antlr4
-  fi
-fi
-
 # generate golang lib
-antlr -Dlanguage=Go -package abslgo -visitor -o abslgo ./ABSLLexer.g4 
-antlr -Dlanguage=Go -package abslgo -visitor -o abslgo ./ABSLParser.g4 
+antlr4 -Dlanguage=Go -package abslgo -visitor -o abslgo ./ABSLLexer.g4 
+antlr4 -Dlanguage=Go -package abslgo -visitor -o abslgo ./ABSLParser.g4 
 
 # generate javascript lib
-antlr -Dlanguage=JavaScript -visitor -package absl -o js ./ABSLLexer.g4 
-antlr -Dlanguage=JavaScript -visitor -package absl -o js ./ABSLParser.g4 
+antlr4 -Dlanguage=JavaScript -visitor -package absl -o js ./ABSLLexer.g4 
+antlr4 -Dlanguage=JavaScript -visitor -package absl -o js ./ABSLParser.g4 
--- bodl/make_lib.sh
+++ bodl/make_lib.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# alias
-if ! type "antlr" > /dev/null; then
-  if type "antlr4" > /dev/null; then 
-    alias antlr=antlr4
-  fi
-fi
-
 # generate golang lib
-antlr -Dlanguage=Go -package bodlgo -visitor -o bodlgo ./BODLLexer.g4 
-antlr -Dlanguage=Go -package bodlgo -visitor -o bodlgo ./BODLParser.g4 
+antlr4 -Dlanguage=Go -package bodlgo -visitor -o bodlgo ./BODLLexer.g4 
+antlr4 -Dlanguage=Go -package bodlgo -visitor -o bodlgo ./BODLParser.g4 
 
 # generate javascript lib
-antlr -Dlanguage=JavaScript -visitor -package bodl -o js ./BODLLexer.g4 
-antlr -Dlanguage=JavaScript -visitor -package bodl -o js ./BODLParser.g4 
+antlr4 -Dlanguage=JavaScript -visitor -package bodl -o js ./BODLLexer.g4 
+antlr4 -Dlanguage=JavaScript -visitor -package bodl -o js ./BODLParser.g4 
